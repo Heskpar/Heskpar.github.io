6f193518ef5bfd4b4a02be104a3e543781031158
kchu17 <39917225+kchu17@users.noreply.github.com>
2019-04-22 22:09:51 -0700

    Created Moveable Rectangle Nodes

--- canvastest2/graphed.js
+++ canvastest2/graphed.js
@@ -4,8 +4,14 @@ function drawGrabber(x, y) {
   const size = 5;
   const panel = document.getElementById('graphpanel')
   const ctx = panel.getContext('2d')
+  ctx.beginPath()
+  ctx.rect(x - size / 2, y - size / 2 , size, size, Math.PI*2, true)
   ctx.fillStyle = 'black'
-  ctx.fillRect(x - size/2, y-size/2, size, size);
+  ctx.fill()
+}
+
+function center(rect) {
+  return { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 }
 }
 
 function createCircleNode (x, y, size, color) {
@@ -26,26 +32,54 @@ function createCircleNode (x, y, size, color) {
       y += dy
     },
     draw: () => {
-      const container = document.getElementById('nodeContainer')
-      const table = document.createElement('table')
-      const tr = document.createElement('tr')
-      const td = document.createElement('td')
-      table.appendChild(tr)
-      tr.appendChild(td)
-      table.style.position = 'absolute'
-      table.style.left = x + 'px'
-      table.style.top = y + 'px'
-      table.style.width = size + 'px'
-      table.style.height = size + 'px'
-      table.style.background = color
-      container.appendChild(table)
-
+      // const container = document.getElementById('nodeContainer')
+      // const table = document.createElement('table')
+      // const tr = document.createElement('tr')
+      // const td = document.createElement('td')
+      // table.appendChild(tr)
+      // tr.appendChild(td)
+      // table.style.position = 'absolute'
+      // table.style.left = x + 'px'
+      // table.style.top = y + 'px'
+      // table.style.width = size + 'px'
+      // table.style.height = size + 'px'
+      // table.style.background = color
+      // container.appendChild(table)
+      const panel = document.getElementById('graphpanel')
+      const ctx = panel.getContext('2d')
+      ctx.beginPath()
+      ctx.arc(x+size/2, y + size / 2, size / 2, 0, Math.PI*2, true)
+      ctx.fillStyle = color
+      ctx.fill()
     }
   }
 }
 
-function center(rect) {
-  return { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 }
+function createRectangleNode (x, y, size) {
+  return {
+    getBounds: () => {
+      return {
+        x: x,
+        y: y,
+        width: size,
+        height: size
+      }
+    },
+    contains: p => {
+      return (x + size / 2 - p.x) ** 2 + (y + size / 2 - p.y) ** 2 <= size ** 2 / 4
+    },
+    translate: (dx, dy) => {
+      x += dx
+      y += dy
+    },
+    draw: () => {
+      const canvas = document.getElementById('graphpanel')
+      const ctx = canvas.getContext('2d') // No need for "if (canvas.getContext)"
+      ctx.beginPath()
+      ctx.rect(x - size, y - size , size, size, Math.PI*2, true)
+      ctx.stroke()
+    }
+  }
 }
 
 function createLineEdge() {
@@ -108,4 +142,93 @@ class Graph {
 
   }
 }
+  document.addEventListener('DOMContentLoaded', function () {
+    const graph = new Graph()
+    const n1 = createCircleNode(10, 10, 20, 'goldenrod')
+    const n2 = createCircleNode(30, 30, 20, 'blue')
+    const n3 = createRectangleNode(50, 30, 50, 10)
+    const n4 = createRectangleNode(30,20, 50, 10);
+    graph.add(n1)
+    graph.add(n2)
+    graph.add(n3)
+    graph.add(n4)
+    const e = createLineEdge()
+    graph.connect(e, { x: 20, y: 20 }, { x: 40, y: 40 })
+    graph.draw()
+    
+    const panel = document.getElementById('graphpanel')
+    let selected = undefined
+    let dragStartPoint = undefined
+    let dragStartBounds = undefined
+  
+    // function repaint(mousePoint) {
+    //   const canvas = document.getElementById('graphpanel')
+    //   const ctx = canvas.getContext('2d')
+    //   ctx.clearRect(0, 0, canvas.width, canvas.height)
+    //   panel.innerHTML = ''
+    //   if (selected !== undefined) {
+    //     const bounds = selected.getBounds()
+    //     drawGrabber(bounds.x, bounds.y)
+    //     drawGrabber(bounds.x + bounds.width, bounds.y)
+    //     drawGrabber(bounds.x, bounds.y + bounds.height)
+    //     drawGrabber(bounds.x + bounds.width, bounds.y + bounds.height)
+    //   }
+    //   if (selected === undefined) {
+    //     const n4 = new circleNode(mousePoint.x, mousePoint.y, 20, 'blue')
+    //     graph.add(n4)
+    //   }
+    //   graph.draw()
+    // }
 
+    function repaint() {
+      const ctx = panel.getContext('2d')
+      ctx.clearRect(0, 0, panel.width, panel.height)
+      panel.innerHTML = ''
+      graph.draw()
+      if (selected !== undefined) {
+        const bounds = selected.getBounds()
+        drawGrabber(bounds.x, bounds.y)
+        drawGrabber(bounds.x + bounds.width, bounds.y)
+        drawGrabber(bounds.x, bounds.y + bounds.height)      
+        drawGrabber(bounds.x + bounds.width, bounds.y + bounds.height)
+      }    
+    }
+    
+    function mouseLocation(event) {
+      var rect = panel.getBoundingClientRect();
+      return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+      }
+    }
+    
+    panel.addEventListener('mousedown', event => {
+      let mousePoint = mouseLocation(event)
+      selected = graph.findNode(mousePoint)
+      if (selected !== undefined) {
+        dragStartPoint = mousePoint
+        dragStartBounds = selected.getBounds(mousePoint)
+      }
+      repaint(mousePoint)
+    })
+  
+    panel.addEventListener('mousemove', event => {
+      if (dragStartPoint === undefined) return
+      let mousePoint = mouseLocation(event)
+      if (selected !== undefined) {
+        const bounds = selected.getBounds();
+        
+        selected.translate(
+          dragStartBounds.x - bounds.x 
+            + mousePoint.x - dragStartPoint.x,
+          dragStartBounds.y - bounds.y 
+            + mousePoint.y - dragStartPoint.y);
+        repaint()
+      }
+    })
+    
+    panel.addEventListener('mouseup', event => {
+      dragStartPoint = undefined
+      dragStartBounds = undefined
+    })
+})
