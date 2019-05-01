22bf5e72cc7ff6b449b859bbdae7bf97d542a4f5
kchu17 <39917225+kchu17@users.noreply.github.com>
2019-05-01 16:46:08 -0700

    changes to edge image

--- framework2/LineEdgeInheritance.js
+++ framework2/LineEdgeInheritance.js
@@ -1,23 +1,23 @@
-'use strict'
+// 'use strict'
 
-// function createLineEdgeInheritance()
-// {
-//     return {
-//         draw: () => {
-//             const canvas = document.getElementById('graphpanel');
-//             const ctx = canvas.getContext('2d');
-//             // Solid line
-//             ctx.beginPath()
-//             ctx.setLineDash([])
-//             ctx.moveTo(0, 100)
-//             ctx.lineTo(300, 100)
-//             ctx.stroke()
-//             ctx.beginPath()
-//             ctx.moveTo(75, 50)
-//             ctx.lineTo(100, 75)
-//             ctx.lineTo(100, 25)
-//             ctx.fill()
-//             ctx.stroke()
-//         }
-//       }
-// }
+// // function createLineEdgeInheritance()
+// // {
+// //     return {
+// //         draw: () => {
+// //             const canvas = document.getElementById('graphpanel');
+// //             const ctx = canvas.getContext('2d');
+// //             // Solid line
+// //             ctx.beginPath()
+// //             ctx.setLineDash([])
+// //             ctx.moveTo(0, 100)
+// //             ctx.lineTo(300, 100)
+// //             ctx.stroke()
+// //             ctx.beginPath()
+// //             ctx.moveTo(75, 50)
+// //             ctx.lineTo(100, 75)
+// //             ctx.lineTo(100, 25)
+// //             ctx.fill()
+// //             ctx.stroke()
+// //         }
+// //       }
+// // }
