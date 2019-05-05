bd7abdee64d86d62e1b5cb7699c6d7e99bdd0df8
mattcrane20 <matthew.crane@sjsu.edu>
2019-05-05 12:59:20 -0700

    Added scaled node buttons, under frameworkmini

---
+++ frameworkmini/ClassNode.js
@@ -0,0 +1,159 @@
+'use strict'
+const ClassNodePrototype = {
+  getBounds: function(){
+    return{x: this.x,y:this.y,width:this.width,height:this.height}
+  },
+
+  contains: function(p){
+    return (this.x <= p.x && p.x <= this.x+this.width) &&
+      (this.y <= p.y && p.y <= this.y + this.height)
+  },
+
+  translate: function(dx, dy){
+    this.x += dx
+    this.y += dy
+  },
+  
+  drawButton: function(canvas){
+    const x = createClassNode(5, 5, 15)
+    x.drawMini(canvas)
+
+    /*const ctx = canvas.getContext('2d')
+    ctx.beginPath()
+    ctx.rect(5 , 6, 30, 15, Math.PI*2, true)
+    ctx.rect(5, 21, 30, 7, Math.PI*2, true)
+    ctx.rect(5, 28, 30, 7, Math.PI*2, true)
+    ctx.stroke()*/
+  },
+
+  drawMini: function(canvas) {
+    const ctx = canvas.getContext('2d') // No need for "if (canvas.getContext)"
+    ctx.beginPath()
+    ctx.rect(this.x, this.y, this.size*2, this.size, Math.PI*2, true)
+    if(this.attribute !== ""){
+      ctx.rect(this.x, this.y + this.size, this.size * 2, this.size / 2, Math.PI*2, true)
+      this.height = this.size * 1.5
+    }
+    if(this.method !== ""){
+      ctx.rect(this.x, this.y + (this.size * 1.5), this.size * 2, this.size / 2, Math.PI*2, true)
+      this.height = this.size * 2
+    }
+
+    ctx.fillStyle = "white"
+    ctx.fillRect(this.x + 1, this.y + 1, (this.size * 2) - 1, this.size - 1, Math.PI*2, true)
+    if(this.attribute !== ""){
+      ctx.fillRect(this.x + 1, (this.y + this.size) + 1, (this.size * 2) - 1, (this.size / 2) - 1,  Math.PI*2, true)
+    }
+    if(this.method !== ""){
+      ctx.fillRect(this.x + 1, (this.y + this.size * 1.5) + 1, (this.size * 2) - 1, (this.size / 2) - 1, Math.PI*2, true)
+    }
+
+    ctx.font = '12px serif'
+    ctx.fillStyle = 'black'
+    ctx.fillText(this.classname, this.x + this.size / 3, this.y + this.size / 1.5)
+    ctx.font = '12px serif'
+    if(this.attribute !== ""){
+      ctx.fillText(this.attribute, this.x + this.size / 2, this.y + (this.size * 1.3))
+    }
+    if(this.method !== ""){
+      ctx.fillText(this.method, this.x + this.size / 2, this.y + (this.size * 1.8))
+    }
+    ctx.stroke()
+  },
+
+  draw: function() {
+    const canvas = document.getElementById('graphpanel')
+    const ctx = canvas.getContext('2d') // No need for "if (canvas.getContext)"
+    ctx.beginPath()
+    ctx.rect(this.x, this.y, this.size*2, this.size, Math.PI*2, true)
+    if(this.attribute !== ""){
+      ctx.rect(this.x, this.y + this.size, this.size * 2, this.size / 2, Math.PI*2, true)
+      this.height = this.size * 1.5
+    }
+    if(this.method !== ""){
+      ctx.rect(this.x, this.y + (this.size * 1.5), this.size * 2, this.size / 2, Math.PI*2, true)
+      this.height = this.size * 2
+    }
+
+    ctx.fillStyle = "white"
+    ctx.fillRect(this.x + 1, this.y + 1, (this.size * 2) - 1, this.size - 1, Math.PI*2, true)
+    if(this.attribute !== ""){
+      ctx.fillRect(this.x + 1, (this.y + this.size) + 1, (this.size * 2) - 1, (this.size / 2) - 1,  Math.PI*2, true)
+    }
+    if(this.method !== ""){
+      ctx.fillRect(this.x + 1, (this.y + this.size * 1.5) + 1, (this.size * 2) - 1, (this.size / 2) - 1, Math.PI*2, true)
+    }
+
+    ctx.font = '24px serif'
+    ctx.fillStyle = 'black'
+    ctx.fillText(this.classname, this.x + this.size / 3, this.y + this.size / 1.5)
+    ctx.font = '12px serif'
+    if(this.attribute !== ""){
+      ctx.fillText(this.attribute, this.x + this.size / 2, this.y + (this.size * 1.3))
+    }
+    if(this.method !== ""){
+      ctx.fillText(this.method, this.x + this.size / 2, this.y + (this.size * 1.8))
+    }
+    ctx.stroke()
+  },
+
+  getConnectionPoint: function(point){
+    var centerX = this.x + this.width/2
+    var centerY = this.y + this.height/2
+    var dx = point.x - centerX
+    var dy = point.y - centerY
+    if(dx<-dy && dx >= dy) return {x: centerX, y: this.y}
+    else if(dx >= -dy && dx >= dy) return  {x: this.x + this.width, y: centerY + (this.height/4)}
+    // else if(dx >= -dy && dx >= dy) return  {x: this.x + this.width}
+    else if(dx < -dy && dx <dy) return {x:this.x,y: centerY}
+    else if(dx >= -dy && dx<dy)return{x: centerX, y:this.y + this.height}
+  },
+
+  center:function()  {
+    return{
+      x: this.x+this.width/2 ,
+      y: this.y+this.height/2
+    }
+  },
+
+  propSheet: function(){
+    document.getElementById("text1").style.visibility = "visible";
+    document.getElementById("label1").style.visibility = "visible";
+    document.getElementById("text2").style.visibility = "visible";
+    document.getElementById("label2").style.visibility = "visible";
+    document.getElementById("text3").style.visibility = "visible";
+    document.getElementById("label3").style.visibility = "visible";
+
+    document.getElementById("colorpicker").style.visibility = "hidden";
+    document.getElementById("label4").style.visibility = "hidden";
+    document.getElementById("label5").style.visibility = "hidden";
+
+
+    if(document.getElementById("text1").value !== ""){
+      this.classname = document.getElementById("text1").value;
+      document.getElementById("text1").value = "";
+    }
+    if(document.getElementById("text2").value !== ""){
+      this.attribute = document.getElementById("text2").value;
+      document.getElementById("text2").value = "";
+    }
+
+    if(document.getElementById("text3").value !== ""){
+      this.method = document.getElementById("text3").value;
+      document.getElementById("text3").value = "";
+    }
+  }
+}
+
+function createClassNode (x, y, size) {
+  const result = Object.create(ClassNodePrototype)
+  result.classname = "Class"
+  result.attribute = ""
+  result.method = ""
+  result.x = x
+  result.y = y
+  result.size = size
+  result.width = size*2
+  result.height = size
+  return result
+}
---
+++ frameworkmini/Graph.js
@@ -0,0 +1,78 @@
+'use strict'
+
+function center(rect) {
+    return { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 }
+}
+
+class Graph {
+    constructor() {
+	this.nodes = []
+	this.edges = []
+    }
+    add(n) {
+	this.nodes.push(n)
+    }
+    findNode(p) {
+	for (let i = this.nodes.length - 1; i >= 0; i--) {
+	    const n = this.nodes[i]
+	    if (n.contains(p)) return n
+	}
+	return undefined
+    }
+
+    connect(e, p1, p2) {
+	const n1 = this.findNode(p1)
+	const n2 = this.findNode(p2)
+	if (n1 !== undefined && n2 !== undefined) {
+	    e.connect(n1, n2)
+	    this.edges.push(e)
+	    return true
+	}
+	return false
+    }
+    draw() {
+	for (const n of this.nodes) {
+	   // n.draw()
+	   n.draw()
+	}
+	for (const e of this.edges) {
+	    e.draw()
+	}
+
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    const graph = new Graph()
+    const toolbar = new ToolBar(graph)
+
+    const n5 = createNoteNode(50, 150, 45, 10)
+
+    toolbar.addNode(createClassNode)
+    toolbar.addNode(createNoteNode)
+    toolbar.addNode(createPackageNode)
+    toolbar.addNode(createInterfaceNode)
+    toolbar.addEdge(createLineEdge)
+
+    graph.add(n5)
+
+    const e = createLineEdge()
+    graph.connect(e, { x: 60, y: 40 }, { x: 220, y: 120 })
+    graph.draw()
+    
+
+    //Testing edge arrows
+   
+//    graph.add(n7)
+  //  graph.add(n8)
+    //const e2 = createLineEdgeDependecy()
+    //graph.add(e2)
+    // graph.connect(e2, { x: 60, y: 40 }, { x: 220, y: 120 })
+    graph.draw()
+    //const e3 = createLineEdgeInheritance()
+    //graph.add(e3)
+    graph.draw()
+
+    const panel = document.getElementById('graphpanel')
+    toolbar.tools[0].Switch()
+        
+})
