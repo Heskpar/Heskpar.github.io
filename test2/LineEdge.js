70cf60b9a0646030530b7a73adf0354953b7e2d1
fish <you@example.com>
2019-05-05 15:32:06 -0700

    try to connect two nodes...

--- test2/LineEdge.js
+++ test2/LineEdge.js
@@ -19,6 +19,41 @@ function createLineEdge() {
       ctx.moveTo(p.x, p.y)
       ctx.lineTo(q.x, q.y)
       ctx.stroke()
+    },
+
+    connectTwoNode: () => {
+      var canvas = document.getElementById("toolbar");
+      var ctx = canvas.getContext("2d");
+      var flag = false;
+      var tc = document.createElement("canvas");
+      tc.width = canvas.width;
+      tc.height = canvas.height;
+      var tctx = tc.getContext("2d");
+      var x = 0;
+      var y = 0;
+
+      canvas.onmousedown = function (event) {
+        event = event || window.event;
+        x = event.clientX - canvas.offsetLeft + (document.body.scrollLeft || document.documentElement.scrollLeft);
+        y = event.clientY - canvas.offsetTop  + (document.body.scrollTop || document.documentElement.scrollTop);
+        flag = true;
+        tctx.drawImage(canvas,0,0,canvas.width,canvas.height);
+      }
+      canvas.onmousemove = function (event) {
+        if (!flag) return;
+        event = event || window.event;
+        var gx = event.clientX - canvas.offsetLeft + (document.body.scrollLeft || document.documentElement.scrollLeft);
+        var gy = event.clientY - canvas.offsetTop  + (document.body.scrollTop || document.documentElement.scrollTop);
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.drawImage(tc,0,0,canvas.width,canvas.height);
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+        ctx.lineTo(gx,gy);
+        ctx.stroke();
+      }
+      canvas.onmouseup = function (e) {
+        flag = false;
+      }
     }
   }
 }
