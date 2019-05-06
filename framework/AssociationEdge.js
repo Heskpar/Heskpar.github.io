13adc43f111d0f9d2c55c5ac526aa89164fdffa1
kchu17 <39917225+kchu17@users.noreply.github.com>
2019-05-06 15:16:19 -0700

    Added in jsdocs for edges

--- framework/AssociationEdge.js
+++ framework/AssociationEdge.js
@@ -1,16 +1,26 @@
 'use strict'
 
+/**
+	 *draw
+	 *This function is used to draw the edge connection between nodes
+	 *@type {number} startPoint The starting coordinates of the node from center
+	 *@type {number} endPoint The ending coordinates of the node from center
+	 *@type {number} dx the distance between the x coordinates of the startPoint and endPoint
+	 *@type {number} dy the distance between the y coordinates of the startPoint and endPoint
+	 *@const {canvas} canvas the grid that the edge will be drawn on
+	 *@const {ctx} ctx the object based of the dge that is used to be drawn or modified
+	 */
 const AssociationEdgePrototype = {
     
     draw: function() {
-	var startPoint = this.startNode.getConnectionPoint(this.endNode.center())
-	var endPoint = this.endNode.getConnectionPoint(startPoint)
+	let startPoint = this.startNode.getConnectionPoint(this.endNode.center())
+	let endPoint = this.endNode.getConnectionPoint(startPoint)
 	const canvas = document.getElementById('graphpanel')
 	const ctx = canvas.getContext('2d')
 	
 
-	var dx = startPoint.x - endPoint.x
-	var dy = startPoint.y - endPoint.y
+	let dx = startPoint.x - endPoint.x
+	let dy = startPoint.y - endPoint.y
 	
 	//if the other node is above or below
 	if((dx>=dy && dx>= -dy)||(dx<dy && dx<-dy)) {
@@ -46,11 +56,14 @@ const AssociationEdgePrototype = {
 	    ctx.stroke()
 	}
 	
+	},
 	
-	
-	
-	
-    },
+	/**
+	 *connect
+	 *Gets the connection between two points and connects the two points
+	 *@param {number} p the point where the connection begins from
+	 *@param {number} q the point where the connection ends at
+	 */
     connect: (p,q) => {
 	const canvas = document.getElementById('graphpanel')
 	const ctx = canvas.getContext('2d')
@@ -63,7 +76,11 @@ const AssociationEdgePrototype = {
 	
     },
 
-
+	/**
+	 * drawButton
+	 * This function takes a cnavas and draws a small representationof what the tool does
+	 * @param {canvas} canvas a 42 by 42 px sized cnvas that will serve as a button
+	 */
     drawButton: (canvas) => {
 	const ctx = canvas.getContext('2d')
 	ctx.beginPath()
@@ -77,7 +94,13 @@ const AssociationEdgePrototype = {
     }
 }
 
-
+/**
+ * Represents the AggregationEdge
+ * @constructor
+ * @param {*} startNode the node the edge begins from
+ * @param {*} endNode the node the edge ends at
+ * @return {AggregationEdgePrototype} an AggregationEdge
+ */
 function createAssociationEdge(startNode,endNode)
 {
     var result = Object.create(AssociationEdgePrototype)
--- framework/InterfaceEdge.js
+++ framework/InterfaceEdge.js
@@ -1,7 +1,17 @@
 'use strict'
 
 const InterfaceEdgePrototype = {
-    
+	
+	/**
+	 * draw
+	 * This function is used to draw the edge connection between nodes
+	 *@type {number} startPoint The starting coordinates of the node from center
+	 *@type {number} endPoint The ending coordinates of the node from center
+	 *@type {number} dx the distance between the x coordinates of the startPoint and endPoint
+	 *@type {number} dy the distance between the y coordinates of the startPoint and endPoint
+	 *@const {canvas} canvas the grid that the edge will be drawn on
+	 *@const {ctx} ctx the object based of the dge that is used to be drawn or modified
+	 */
     draw: function() {
 	var startPoint = this.startNode.getConnectionPoint(this.endNode.center())
 	var endPoint = this.endNode.getConnectionPoint(startPoint)
@@ -49,11 +59,14 @@ const InterfaceEdgePrototype = {
 	    ctx.stroke()
 	}
 	
+	},
 	
-	
-	
-	
-    },
+	/**
+	 *connect
+	 *Gets the connection between two points and connects the two points
+	 *@param {*} p the point where the connection begins from
+	 *@param {*} q the point where the connection ends at
+	 */
     connect: (p,q) => {
 	const canvas = document.getElementById('graphpanel')
 	const ctx = canvas.getContext('2d')
@@ -66,7 +79,11 @@ const InterfaceEdgePrototype = {
 	
     },
 
-
+	/**
+	 * drawButton
+	 * This function takes a cnavas and draws a small representationof what the tool does
+	 * @param {canvas} canvas a 42 by 42 px sized cnvas that will serve as a button
+	 */
     drawButton: (canvas) => {
 	const ctx = canvas.getContext('2d')
 	ctx.beginPath()
@@ -84,7 +101,13 @@ const InterfaceEdgePrototype = {
     }
 }
 
-
+/**
+ * Represents the AggregationEdge
+ * @constructor
+ * @param {*} startNode the node the edge begins from
+ * @param {*} endNode the node the edge ends at
+ * @return {Object} an AggregationEdge
+ */
 function createInterfaceEdge(startNode,endNode)
 {
     var result = Object.create(InterfaceEdgePrototype)
