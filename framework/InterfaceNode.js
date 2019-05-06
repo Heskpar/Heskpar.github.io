dfba6681a18a39c66db515e8d69b814a635248f6
mattcrane20 <matthew.crane@sjsu.edu>
2019-05-06 15:30:10 -0700

    modified:   InterfaceNode.js

--- framework/InterfaceNode.js
+++ framework/InterfaceNode.js
@@ -1,19 +1,45 @@
 'use strict'
 const InterfaceNodePrototype = {
+
+  /**
+   * Returns the bounds of the object, including height and width
+   * @returns {number} x - x-axis coord
+   * @returns {number} y - y-axis coord
+   * @returns {number} width - size of width
+   * @returns {number} height - size of height
+   */
   getBounds: function(){
     return{x: this.x,y:this.y,width:this.width,height:this.height}
   },
 
+  /**
+   * Takes a point and determines whether the point is within the bounds of the node.
+   * @param {point} p - point on canvas to be determined
+   * @returns {boolean} - whether the node contains the point
+   */
   contains: function(p){
     return (this.x <= p.x && p.x <= this.x+this.width) &&
     (this.y <= p.y && p.y <= this.y + this.height)
   },
 
+  /**
+   * Based on the position of the other connection point (point),
+   * calculates second point on node for edge to connect to.
+   * @param {point} point - connection point on another node, start of an edge.
+   * @returns {number} x - x coordinate for second point.
+   * @returns {number} y - y coordinate for second point.
+   */
   translate: function(dx, dy){
     this.x += dx
     this.y += dy
   },
 
+  /** drawbutton
+   *This function takes a canvas, and draws a small representation
+   *of what the tool does.
+   *@param {canvas} canvas - this argument is expected to be a small, 
+   *42 by 42 px sized canvas that will serve as a button. 
+   */
   drawButton: function(canvas){
     const ctx = canvas.getContext('2d')
     ctx.beginPath()
@@ -23,10 +49,9 @@ const InterfaceNodePrototype = {
   },
 
   /**
-   * draw 
-   * This function draws the object node based on the set height and width
-   * @const {canvs} canvas the grid that the node will be drawn on 
-   * @const {ctx} ctx the object that is drawn or modified on the grid
+   * Draws the canvas node element onto the main area (graphpanel), based on x and y for position, and 
+   * height and width for size.
+   * Also draws or omits certain parts of the node based on property values.
    */
   draw: function() {
     const canvas = document.getElementById('graphpanel')
@@ -54,6 +79,13 @@ const InterfaceNodePrototype = {
     ctx.stroke()
   },
 
+  /**
+   * Based on the position of the other connection point (point),
+   * calculates second point on node for edge to connect to.
+   * @param {point} point - connection point on another node, start of an edge.
+   * @returns {number} x - x coordinate for second point.
+   * @returns {number} y - y coordinate for second point.
+   */
   getConnectionPoint: function(point){
     var centerX = this.x + this.width/2
     var centerY = this.y + this.height/2
@@ -66,6 +98,11 @@ const InterfaceNodePrototype = {
     else if(dx >= -dy && dx<dy)return{x: centerX, y:this.y+this.height}
   },
 
+  /**
+   * Returns the center of the node based on x, y, width, and height.
+   * @returns {number} x - x-axis coordinate of center.
+   * @returns {number} y - y-axis coordinate of center.
+   */
   center:function()  {
     return{
       x: this.x+this.width/2 ,
@@ -73,6 +110,11 @@ const InterfaceNodePrototype = {
     }
   },
 
+ /**
+   * Sets all necessary HTML elements for property editing to visible.
+   * Based on entry from user into the applicable fields for this node,
+   * changes necessary properties.
+   */
   propSheet: function() {
     document.getElementById("text1").style.visibility = "visible";
     document.getElementById("label1").style.visibility = "visible";
@@ -97,6 +139,13 @@ const InterfaceNodePrototype = {
   }
 }
 
+/**
+ * Creates an interface class node based on the x and y position, and size.
+ * @param {number} x - x-axis coordinate of new node
+ * @param {number} y - y-axis coordinate of new node
+ * @param {number} size - size of node, used for calculating width and height
+ * @returns {Object} result - the newly created InterfaceNode
+ */
 function createInterfaceNode (x, y, size) {
   const result = Object.create(InterfaceNodePrototype)
   result.name = '<<Interface>>'
