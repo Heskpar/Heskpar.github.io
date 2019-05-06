a7c5559136fbf06efa9bd29b7e54914511777579
mattcrane20 <matthew.crane@sjsu.edu>
2019-05-06 15:39:38 -0700

    jsdoc packagenode

--- framework/PackageNode.js
+++ framework/PackageNode.js
@@ -1,21 +1,46 @@
 'use strict'
 const  PackagePrototype ={
 
+  /**
+   * Returns the bounds of the object, including height and width
+   * @returns {number} x - x-axis coord
+   * @returns {number} y - y-axis coord
+   * @returns {number} width - size of width of the bottom rectangle
+   * @returns {number} height - size of height of both bottom and top rectangle
+   */
   getBounds: function(){
     return{x: this.x,y:this.y,width: this.bottomWidth,
     height:this.bottomHeight + this.topHeight}
   },
 
+  /**
+   * Takes a point and determines whether the point is within the bounds of the node.
+   * @param {point} p - point on canvas to be determined
+   * @returns {boolean} - whether the node contains the point
+   */
   contains: function(p){
     return (this.x <= p.x && p.x <= this.x+this.bottomWidth) &&
       (this.y <= p.y && p.y <= this.y + this.bottomHeight + this.topHeight)
   },
   
+  /**
+   * Based on the position of the other connection point (point),
+   * calculates second point on node for edge to connect to.
+   * @param {point} point - connection point on another node, start of an edge.
+   * @returns {number} x - x coordinate for second point.
+   * @returns {number} y - y coordinate for second point.
+   */
   translate: function(dx, dy){
     this.x += dx
     this.y += dy
   },
   
+  /** drawbutton
+   *This function takes a canvas, and draws a small representation
+   *of what the tool does.
+   *@param {canvas} canvas - this argument is expected to be a small, 
+   *42 by 42 px sized canvas that will serve as a button. 
+   */
   drawButton: function(canvas){
     const ctx = canvas.getContext('2d')
     ctx.beginPath()
@@ -24,6 +49,11 @@ const  PackagePrototype ={
     ctx.stroke()
   },
   
+  /**
+   * Draws the canvas node element onto the main area (graphpanel), based on x and y for position, and 
+   * height and width for size.
+   * Also draws or omits certain parts of the node based on property values.
+   */
   draw: function(){
     const canvas = document.getElementById('graphpanel')
     const ctx = canvas.getContext('2d') // No need for "if (canvas.getContext)"
@@ -41,6 +71,13 @@ const  PackagePrototype ={
     ctx.stroke()
   },
 
+  /**
+   * Based on the position of the other connection point (point),
+   * calculates second point on node for edge to connect to.
+   * @param {point} point - connection point on another node, start of an edge.
+   * @returns {number} x - x coordinate for second point.
+   * @returns {number} y - y coordinate for second point.
+   */
   getConnectionPoint: function(point){
     var centerX = this.x + this.bottomWidth/2
     var centerY = this.y + this.bottomHeight/2
@@ -53,6 +90,11 @@ const  PackagePrototype ={
     else if(dx >= -dy && dx<dy)return{x: centerX, y:this.y + this.bottomHeight +15}
   },
 
+  /**
+   * Returns the center of the node based on x, y, width, and height.
+   * @returns {number} x - x-axis coordinate of center.
+   * @returns {number} y - y-axis coordinate of center.
+   */
   center:function()  {
     return{
       x: this.x +this.bottomWidth,
@@ -60,6 +102,11 @@ const  PackagePrototype ={
     }
   },
     
+  /**
+   * Sets all necessary HTML elements for property editing to visible.
+   * Based on entry from user into the applicable fields for this node,
+   * changes necessary properties.
+   */
   propSheet: function(){
     document.getElementById("text1").style.visibility = "visible";
     document.getElementById("label1").style.visibility = "visible";
@@ -86,6 +133,13 @@ const  PackagePrototype ={
   }
 }
 
+/**
+ * Returns a package node, based on position of x and y, and height and width of size.
+ * @param {number} x - x-axis coordinate of new node
+ * @param {number} y - y-axis coordinate of new node
+ * @param {number} size - size of node, used for calculating top and bottom width and height
+ * @returns {Object} result - the newly created PackageNode
+ */
 function createPackageNode(x, y, size) {
     const result = Object.create(PackagePrototype)
     result.content = 'Package'
